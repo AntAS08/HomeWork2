a1b276526cc4485e73c0358283f6460bae5257cd
AntAS08 <anton-simonenko@inbox.ru>
2025-12-10 00:04:08 +1000

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class  Main {
+    public static void  main(String[] args) {
+       System.out.println("Задача 1");
+        byte i = 10;
+        System.out.println(i);
+        short a = 300;
+        System.out.println(a);
+        int b = -1000000000;
+        System.out.println(b);
+        long c =9000000000000L;
+        System.out.println(c);
+        float e = 1.5f;
+        System.out.println(e);
+        double f = -1.7;
+        System.out.println(f);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Задача 2");
+        float  g = 27.12f;
+        System.out.println(g);
+        long h = 987678965549L;
+        System.out.println(h);
+        double j = 2.286;
+        System.out.println(j);
+        short k = 569;
+        System.out.println(k);
+        short p =-159;
+        System.out.println(p);
+        int m = 27897;
+        System.out.println(m);
+        byte y = 67;
+        System.out.println(y);
     }
 }
